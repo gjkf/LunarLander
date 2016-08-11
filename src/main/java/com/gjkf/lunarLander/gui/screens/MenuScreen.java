b411accdd9a3d537a73114f21c15d9edb24b17ef
gjkf <cossu.dvd@gmail.com>
2016-08-11 12:35:11 +0200

    Updated the engine.

--- src/main/java/com/gjkf/lunarLander/gui/screens/MenuScreen.java
+++ src/main/java/com/gjkf/lunarLander/gui/screens/MenuScreen.java
@@ -4,6 +4,13 @@
 package com.gjkf.lunarLander.gui.screens;
 
 import com.gjkf.seriousEngine.core.gui.GuiScreenWidget;
+import com.gjkf.seriousEngine.core.math.Vector2f;
+import com.gjkf.seriousEngine.core.math.Vector4f;
+import com.gjkf.seriousEngine.core.render.Colors;
+import com.gjkf.seriousEngine.core.render.Renderer;
+import org.lwjgl.opengl.GL11;
+
+import java.util.HashMap;
 
 public class MenuScreen extends GuiScreenWidget{
 
@@ -13,7 +20,14 @@ public class MenuScreen extends GuiScreenWidget{
 
     @Override
     public void drawBackground(){
-
+        HashMap<String, Object> m = new HashMap<>();
+        m.put("uResolution", new Vector2f(450, 350));
+        m.put("uStartingColor", new Vector4f(1f, .6f, .0f, 1f));
+        m.put("uEndingColor", new Vector4f(0f, .6f, .6f, 1f));
+        m.put("uDirection", 0);
+        Renderer.loadShader("shaders/defaultVertex.glsl", "shaders/gradient.glsl", m,
+                () -> Renderer.drawArray(new float[]{150,150, 250,420, 600,500}, Colors.GREEN.color, GL11.GL_TRIANGLES)
+        );
     }
 
     @Override
